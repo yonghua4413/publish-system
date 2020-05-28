28793548800a36c870f4f0fb841ccc2f46acabf7
yonghua4413 <254274509@qq.com>
2020-05-28 18:54:12 +0800

    调试

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
 <?php
 
 Route::middleware([\App\Http\Middleware\CommonMiddleware::class])->group(function (){
-
     Route::get('/', 'HomeController@index');
-
     Route::get('/user/reg.html', 'UserController@reg');
     Route::get('/user/forget.html', 'UserController@forget');
     Route::get('/user/login.html', 'UserController@login');
-
     Route::get('/category/{spell}.html', 'CategoryController@showCategory')->where('spell', '[A-Za-z]+');
-
     Route::get('/publish/detail/{id}.html', 'PublishController@showDetail')->where('id', '[0-9]+');
-
 });
 
 Route::post('/user/doLogin', 'UserController@doLogin');
 Route::get('/user/loginOut', 'UserController@loginOut');
 Route::post('/user/doReg', 'UserController@doReg');
-
 Route::get('/publish/add.html', 'PublishController@showAdd');
