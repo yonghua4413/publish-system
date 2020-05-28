4bd177d267af4c1de3b4595c908722c09a94de13
One_9 <lwy2mx@gmail.com>
2020-05-28 15:42:42 +0800

    fix

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -2,3 +2,4 @@
     <p> 活动阁社区 {{date("Y")}} &copy; <a href="http://www.huodongge.cn" target="_blank">贵州赛雷互动科技有限公司出品</a></p>
     <p>黔ICP备16003751号-11</p>
 </div>
+
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ Route::middleware([\App\Http\Middleware\CommonMiddleware::class])->group(functio
     Route::get('/user/reg.html', 'UserController@reg');
     Route::get('/user/forget.html', 'UserController@forget');
     Route::get('/user/login.html', 'UserController@login');
-
+    Route::get('/user/set.html', 'UserController@set');
+    Route::get('/user/home.html', 'UserController@home');
+    Route::get('/user/home/{id}', 'UserController@home');
 });
 
 Route::post('/user/doLogin', 'UserController@doLogin');
 Route::get('/user/loginOut', 'UserController@loginOut');
 Route::post('/user/doReg', 'UserController@doReg');
+Route::post('/user/rePass', 'UserController@rePass');
+Route::post('/user/setHeadImg', 'UserController@setUserHeadImg');
 
